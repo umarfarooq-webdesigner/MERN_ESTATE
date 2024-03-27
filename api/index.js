7f1e43e0ce286c5f8cf8d31880ccd928bca7ae03
Umar Farooq <106931130+umarfarooq-webdesigner@users.noreply.github.com>
2024-03-27 12:36:55 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,31 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+mongoose.connect(process.env.mongoUrl)
+.then( () => {
+    console.log('Connected to MongoDB');
+})
+.catch( (err) => {
+    console.log('Error connecting to MongoDB:', err)
+});
+ 
+
+
+
+
+
+
+
+
+
 
 const app = express();
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
