7f71117f1ef07bf8f4f16cee1cd1f983bec040ff
Umar Farooq <106931130+umarfarooq-webdesigner@users.noreply.github.com>
2024-03-29 20:37:11 +0500

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,17 @@
 import User from '../models/user.model.js';
-import bcryptjs from 'bcrypt';
+import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-// import { errorHandler } from '../utils/error.js';
+import { errorHandler } from '../utils/error.js';
 
 
 export const signup = async (req, res, next) =>{
     const { username, email, password } = req.body;
+    const saltRounds = 10; 
     //syn password 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword});
-
+    
     try{
+        const hashedPassword = bcrypt.hashSync(password, saltRounds);
+        const newUser = new User({ username, email, password: hashedPassword });
         
         await newUser.save();    
         res.status(201).json("User created successfully!");
@@ -28,7 +29,7 @@ export const signin = async(req, res, next) =>{
     try{
         const validUser = await User.findOne({ email });
         if(!validUser) return next(errorHandler (404, 'User not found'));
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        const validPassword = bcrypt.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler (401, 'Wrong credentials!'));
 
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
